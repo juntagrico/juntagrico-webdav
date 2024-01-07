de3675b611380dd16ebba299f483d9dee60e48fb
pascalfree <pascal.free@ihdg.ch>
2024-01-07 21:35:41 +0100

    upgrade for juntagrico 1.6.0

--- testsettings.py
+++ testsettings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'juntagrico',
+    'import_export',
     'juntagrico_webdav',
 ]
 
@@ -94,6 +95,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
+                'django.template.context_processors.request',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
--- testurls.py
+++ testurls.py
@@ -1,13 +1,11 @@
 """
 test URL Configuration for juntagrico_webdav development
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
-import juntagrico
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('juntagrico.urls')),
-    url(r'^', include('juntagrico_webdav.urls')),
-    url(r'^$', juntagrico.views.home),
+    path('admin/', admin.site.urls),
+    path('', include('juntagrico.urls')),
+    path('', include('juntagrico_webdav.urls')),
 ]
