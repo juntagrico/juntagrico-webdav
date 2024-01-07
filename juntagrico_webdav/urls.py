4c3290b6914996f2d29dc60c2e4fea744f4dcd04
pascalfree <pascal.free@ihdg.ch>
2024-01-07 22:26:28 +0100

    use named urls and static template tag

--- juntagrico_webdav/urls.py
+++ juntagrico_webdav/urls.py
@@ -3,6 +3,6 @@ from juntagrico_webdav import views
 
 app_name = 'webdav'
 urlpatterns = [
-    path('wd/list/<int:id>/', views.list),
+    path('wd/list/<int:id>/', views.list, name='list'),
     path('wd/get/<int:id>/<str:file>/', views.get_item, name='get-file')
 ]
